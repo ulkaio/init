ac6566867423c0f1fedd9f60441ad347848b1ac7
Madhu Banik <mabanik@microsoft.com>
2021-03-11 18:21:14 -0800

    saving

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 echo "Install brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/ulka/.zprofile
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> $HOME/.zprofile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-brew install go gh
+brew install go gh zsh
+
+echo "Install oh my zsh"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Auth login"
 gh auth login
 
 mkdir -p ~/x/src/github.com/ulkaio
@@ -10,5 +15,6 @@ export GOPATH=$HOME/x
 export PATH=$PATH:$GOPATH/bin
 cd $GOPATH/src/github.com/ulkaio
 gh repo clone setup
-go install $GOPATH/src/github.com/ulkaio/setup
+cd setup
+go install
 setup
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
+/bin/bash -c "$(curl -fsSL i.ulka.io)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ulkaio/init/master/init.sh)"
